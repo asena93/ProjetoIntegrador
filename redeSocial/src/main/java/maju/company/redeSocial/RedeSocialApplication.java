8eb6db78888dc8ee1412e394966df97c86f7406d
asena93 <sandro_13moreira@hotmail.com>
2021-04-30 10:53:16 -0300

    ProjetoIntegrador

---
+++ redeSocial/src/main/java/maju/company/redeSocial/RedeSocialApplication.java
@@ -0,0 +1,13 @@
+package maju.company.redeSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeSocialApplication.class, args);
+	}
+
+}
---
+++ redeSocial/src/test/java/maju/company/redeSocial/RedeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package maju.company.redeSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
