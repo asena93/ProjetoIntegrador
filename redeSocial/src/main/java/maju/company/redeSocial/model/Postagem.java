09abd9a3d247e3fc845e25c9210c77df78378af7
Rodrigo Dias <80522229+RodrigoPDias@users.noreply.github.com>
2021-04-30 12:26:22 -0300

    ProjetoIntegrador

---
+++ redeSocial/src/main/java/maju/company/redeSocial/model/Postagem.java
@@ -0,0 +1,56 @@
+package maju.company.redeSocial.model;
+
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+public class Postagem {
+	@Id	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String titulo;
+	
+	@NotNull
+	@Size(min = 5, max = 1000)
+	private String texto;	
+
+	@Temporal(TemporalType.TIMESTAMP)
+    private Date data = new java.sql.Date(System.currentTimeMillis());
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	
+	
+}
