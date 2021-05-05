480d5fc071eb317d0532953f6eba948d661e6fa7
Rodrigo Dias <80522229+RodrigoPDias@users.noreply.github.com>
2021-05-05 09:34:35 -0300

    crud

--- redeSocial/src/main/java/maju/company/redeSocial/model/Postagem.java
+++ redeSocial/src/main/java/maju/company/redeSocial/model/Postagem.java
@@ -2,14 +2,20 @@ package maju.company.redeSocial.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
+@Entity
+@Table(name = "tb_postagem")
 public class Postagem {
 	@Id	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,6 +31,10 @@ public class Postagem {
 
 	@Temporal(TemporalType.TIMESTAMP)
     private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public long getId() {
 		return id;
@@ -50,7 +60,4 @@ public class Postagem {
 	public void setData(Date data) {
 		this.data = data;
 	}
-	
-	
-	
 }
